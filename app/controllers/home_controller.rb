e206e34bc6a8fc7f9bd6adf1c93119f553df8014
ryanthomas92 <rthoma11@villanova.edu>
2017-02-22 16:06:06 -0800

    got scraped results stored in an instance variable array

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,11 +5,11 @@ class HomeController < ApplicationController
 
   def index
     page = Nokogiri::HTML(open("http://www.thegolfcourses.net/golfcourses/NJ/NewJersey.htm"))
-    @courses = page.css('li a')
-    course_array = []
-    @courses.map do |a|
+    courses = page.css('ol a')
+    @course_array = []
+    courses.map do |a|
       course_name = a.text
-      course_array.push(course_name)
+      @course_array.push(course_name)
     end
   end
 end

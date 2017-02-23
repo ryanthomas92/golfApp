40eb159b40abdfd48c3e9db422f70f00aab07631
ryanthomas92 <rthoma11@villanova.edu>
2017-02-23 15:36:54 -0800

    modified routes and controller for user information

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,37 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def show
+    @user = User.find_by_id(params[:id])
+  end
+
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      login(@user)
+      redirect_to @user
+    else
+      redirect_to '/'
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to '/'
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
+
+  get '/users', to: 'users#index'
+  get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create'
+  get '/users/:id', to: 'users#show', as: 'user'
+  delete '/users/:id', to: 'users#destroy'
+
 end

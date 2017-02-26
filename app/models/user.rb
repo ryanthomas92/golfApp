9d5b2ae2963c8a6db4a5173d4cc35167dc93ea5e
ryanthomas92 <rthoma11@villanova.edu>
2017-02-26 13:46:36 -0800

    created routes for login, logout, and signup and controllers

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   has_secure_password
   # validates_uniqueness_of :username, :email
 
+  def self.confirm(params)
+    @user = User.find_by({email: params[:email]})
+    @user ? @user.authenticate(params[:password]) : false
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'home#index'
 
-  get '/users', to: 'users#index'
+  # get '/users', to: 'users#index'
   get '/users/new', to: 'users#new', as: 'new_user'
-  post '/users', to: 'users#create'
+  # post '/users', to: 'users#create'
   get '/users/:id', to: 'users#show', as: 'user'
-  delete '/users/:id', to: 'users#destroy'
+  # delete '/users/:id', to: 'users#destroy'
 
   get '/courses', to: 'courses#index'
   get '/courses/new', to: 'courses#new', as: 'new_course'
@@ -15,5 +15,7 @@ Rails.application.routes.draw do
   delete '/courses/:id', to: 'courses#destroy'
 
   get '/login', to: 'sessions#new'
+  get '/logout', to: 'sessions#destroy'
+  post '/sessions', to: 'sessions#create'
 
 end

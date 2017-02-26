56d648b63dcd7099cfdd14cb23311911bfcdca02
ryanthomas92 <rthoma11@villanova.edu>
2017-02-26 13:57:32 -0800

    added html.erb views to test login/logout/signup, all working

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:username, :email, :password)
+    params.require(:user).permit(:first_name, :last_name, :username, :email, :password)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # get '/users', to: 'users#index'
   get '/users/new', to: 'users#new', as: 'new_user'
-  # post '/users', to: 'users#create'
+  post '/users', to: 'users#create'
   get '/users/:id', to: 'users#show', as: 'user'
   # delete '/users/:id', to: 'users#destroy'
 
